065feaa1b75d64f7aa72166a639e29b1a92cfeb2
Aaron Murray <aaron@omv-extras.org>
2014-02-10 13:17:47 -0600

    update for kralizec

--- var/www/openmediavault/js/omv/module/admin/service/plexmediaserver/Settings.js
+++ var/www/openmediavault/js/omv/module/admin/service/plexmediaserver/Settings.js
@@ -1,22 +1,22 @@
 /**
- * This file is part of OpenMediaVault.
- *
  * @license   http://www.gnu.org/licenses/gpl.html GPL Version 3
+ * @author    Volker Theile <volker.theile@openmediavault.org>
  * @author    OpenMediaVault Plugin Developers <plugins@omv-extras.org>
- * @copyright Copyright (c) 2013 OpenMediaVault Plugin Developers
+ * @copyright Copyright (c) 2009-2013 Volker Theile
+ * @copyright Copyright (c) 2013-2014 OpenMediaVault Plugin Developers
  *
- * OpenMediaVault is free software: you can redistribute it and/or modify
+ * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * any later version.
  *
- * OpenMediaVault is distributed in the hope that it will be useful,
+ * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with OpenMediaVault. If not, see <http://www.gnu.org/licenses/>.
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
  */
 // require("js/omv/WorkspaceManager.js")
 // require("js/omv/workspace/form/Panel.js")
@@ -138,10 +138,8 @@ Ext.define("OMV.module.admin.service.plexmediaserver.Settings", {
             handler : function() {
                 var link = 'http://' + location.hostname + ':32400/manage';
                 window.open(link, '_blank');
-            }
-        },{
-            border : false,
-            html   : "</p>"
+            },
+            margin  : "0 0 5 0"
         }]
       }];
    }
